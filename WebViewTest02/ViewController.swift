d27892ad2d5b4a7dabdd4ef4dbe34bb7851f6039
D7703_16 <d7703_16@d7703-16ui-imac.local>
2019-09-09 14:18:44 +0900

    Initial Commit

---
+++ WebViewTest02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebViewTest02
+//
+//  Created by D7703_16 on 2019. 9. 9..
+//  Copyright © 2019년 201526109. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
